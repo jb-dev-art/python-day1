f64d50560f38f313767dcbe62d77df009308eaa0
jb-dev-art <longgamelearning@gmail.com>
2026-05-01 02:42:52 +0000

    Day 1: First Python Program

---
+++ app.py
@@ -0,0 +1,8 @@
+name = input("What is your name? ")
+cats = input("How many cats do you have? ")
+
+print(f"Hello {name}")
+print(f"You have {1} cat")
+print(f"Maybe next year you will have {int(cats) + 1} cats")
+color = input("What is your favorite color cat? ")
+print(f"Nice, I like {color} cats too!")
