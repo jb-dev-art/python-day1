289f3bab99f26d6d825367d96e31973b8ef188d1
jb-dev-art <longgamelearning@gmail.com>
2026-05-02 18:32:15 -0700

    Day 2: add if else logic

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
-name = input("What is your name? ")
-cats = input("How many cats do you have? ")
+name = input("What is your cat's name? ")
+age = int(input("How old is your cat? "))
 
 print(f"Hello {name}")
-print(f"You have {1} cat")
-print(f"Maybe next year you will have {int(cats) + 1} cats")
-color = input("What is your favorite color cat? ")
-print(f"Nice, I like {color} cats too!")
+
+if age < 5:
+    print("Your cat is under 5 years old.")
+else:
+    print("Your cat is over 5 years old.")
+
+print(f"Next year your cat will be {age + 1}")
