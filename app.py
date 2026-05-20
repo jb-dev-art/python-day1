fc41447def331cc6128568d9c3eed0fa230d85af
jb-dev-art <longgamelearning@gmail.com>
2026-05-20 11:30:48 -0700

    day3 app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
-name = input("What is your cat's name? ")
-age = int(input("How old is your cat? "))
+cat's name = input("What is your cat's name? ")
+cat's age = int(input("How old is your cat? "))
 
 print(f"Hello, owner of {name}")
 
@@ -9,3 +9,10 @@ else:
     print("Your cat is over 5 years old.")
 
 print(f"Next year your cat will be {age + 1}")
+name = "Luna"
+favorite_food = "coffee"
+dream_jobs = "Artist, AI developer"
+
+print(name)
+print(favorite_food)
+print(dream_jobs)
